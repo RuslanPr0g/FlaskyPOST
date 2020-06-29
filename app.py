cd23ef43652a6c8295ae29acee7e93b355d3788e
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-06-29 22:39:06 +0300

    templates added + fix styles

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Ruslan',
+        'title': 'Welcome',
+        'content': 'This is the first post.',
+        'date_posted': 'June 29, 2020'
+    },
+    {
+        'author': 'Vlad',
+        'title': 'Join',
+        'content': 'WOw, I have joined here!',
+        'date_posted': 'June 30, 2020'
+    }
+]
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
 
 if __name__ == '__main__':
