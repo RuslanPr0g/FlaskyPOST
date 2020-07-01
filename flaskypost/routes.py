fba80813bb6372f31af901bebfca3b8d9fe1446d
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-07-01 16:48:13 +0300

    v1

--- flaskypost/routes.py
+++ flaskypost/routes.py
@@ -126,3 +126,15 @@ def update_post(post_id):
         form.title.data = post.title
         form.content.data = post.content
     return render_template('newPost.html', form=form, legend='Post Update')
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Successfully!', 'success')
+    return redirect(url_for('index'))
