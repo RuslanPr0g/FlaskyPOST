a444ec19541e159844c5303297a14584892d4c98
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-07-02 17:52:47 +0300

    reset password fix

--- flaskypost/models.py
+++ flaskypost/models.py
@@ -19,7 +19,7 @@ class User(db.Model, UserMixin):
 
     def get_reset_token(self, expires_sec=120):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': seld.id}).decode('utf-8')
+        return s.dumps({'user_id': self.id}).decode('utf-8')
 
     @staticmethod
     def verify_reset_token(token):
