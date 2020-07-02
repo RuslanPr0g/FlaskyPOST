d25e581431d9f1bae81510af959fb7d1cf345ffd
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-07-02 21:22:48 +0300

    setting up user bp

--- flaskypost/users/routes.py
+++ flaskypost/users/routes.py
@@ -1,3 +1,104 @@
 from flask import Blueprint
 
 users = Blueprint('users', __name__)
+
+
+@users.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = UpdateProfileForm()
+    if form.validate_on_submit():
+        if form.image.data:
+            _img = save_image_for_user(form.image.data)
+            current_user.image = _img
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Successfully!', 'success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for(
+        'static', filename='profile_images/' + current_user.image)
+    return render_template('profile.html', image_file=image_file, form=form)
+
+
+@users.route('/user/<string:username>')
+def user(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(
+        Post.date_posted.desc()).paginate(page=page, per_page=9)
+    return render_template('user_page.html', posts=posts, user=user)
+
+
+@users.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Successfully!', 'success')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
+
+
+@users.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+        else:
+            flash('Wrong email or password.', 'danger')
+    return render_template('login.html', form=form)
+
+
+@users.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@users.route('/password_reset', methods=['GET', 'POST'])
+def password_reset():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_token_by_email(user)
+        flash('Please, check your email.', 'success')
+        return redirect(url_for('login'))
+    return render_template('resetPassword.html', form=form)
+
+
+@users.route('/password_reset/<token>', methods=['GET', 'POST'])
+def password_reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('Time expired or you do not have the access.', 'warning')
+        return redirect(url_for('password_reset'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Successfully!', 'success')
+        return redirect(url_for('login'))
+    return render_template('password_reset_token.html', form=form)
---
+++ flaskypost/users/utils.py
@@ -0,0 +1,23 @@
+def save_image_for_user(_image):
+    rand_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(_image.filename)
+    img_fn = rand_hex + f_ext
+    img_path = os.path.join(app.root_path, 'static/profile_images', img_fn)
+
+    output_size = (125, 125)
+    i = Image.open(_image)
+    i.thumbnail(output_size)
+    i.save(img_path)
+
+    return img_fn
+
+
+def send_token_by_email(user):
+    token = user.get_reset_token()
+    message = Message('Reset Password Request.',
+                      sender='theruslanrudenko1992@gmail.com', recipients=[user.email])
+    message.body = f''' 
+Reset Password Link: {url_for('password_reset', token=token, _external=True)}
+If you haven't made this request, just ignore it.
+'''
+    mail.send(message)
