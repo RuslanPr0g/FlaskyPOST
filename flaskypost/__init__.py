1319a0e0efa3ebcfb0c6b4aa796891d41673246a
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-07-02 22:30:58 +0300

    fix envior

--- flaskypost/__init__.py
+++ flaskypost/__init__.py
@@ -1,22 +1,16 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskypost.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '25613c65532b1973abc984a0faa7aecf'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flpst.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 
